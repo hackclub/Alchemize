ce2d0f7753af0df78e40b0dd5bc80d67323eec1a
CoolCream <mahasankarshant@gmail.com>
2026-06-19 20:27:52 +0530

    turned off

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
 	import "./layout.css"
 
-  import { Toaster } from "$lib/components/ui/sonner/index.js";
+	import { Toaster } from "$lib/components/ui/sonner/index.js"
 	let { children } = $props()
-
 </script>
-<svelte:head><link rel="icon" href="/Alchemist.webp" />
-<title>Alchemize</title>
+
+<svelte:head
+	><link rel="icon" href="/Alchemist.webp" />
+	<title>Alchemize</title>
 </svelte:head>
-<Toaster position="top-center" richColors expand={true}/>
+<Toaster position="top-center" richColors expand={true} />
 {@render children()}
--- src/routes/turned-off/+page.svelte
+++ src/routes/turned-off/+page.svelte
@@ -1,3 +1,17 @@
-<div class="" style="text-align: center; padding: 2em;">
-    Heya! While we conduct some mandatory maintenance, the dashboard is temporarily unavailable. Feel free to read our <a href="/docs" style="color:red;">documentation</a> while you wait.
+<script>
+	import Button from "$lib/components/ui/button/button.svelte"
+</script>
+
+<div
+	class="h-screen w-full bg-gradbg flex flex-col gap-y-5 items-center justify-center p-5"
+>
+	<p class="max-w-3xl text-center text-xl text-admin-text">
+		Heya! While we conduct some mandatory maintenance, the dashboard is
+		temporarily unavailable. Feel free to read our documentation while you wait
+	</p>
+	<a href="/docs">
+		<Button class="p-5 px-15 text-xl hover:-translate-y-px hover:bg-primary/70">
+			Read the docs
+		</Button>
+	</a>
 </div>
